f163bc0da477b905ece6db4fe104ae164d9a764b
n0psledbyte <n0psledbyte@gmail.com>
2020-04-04 11:58:13 +0700

    add percobaan method c#

---
+++ modul4_kel02_method_c#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace modul4_kel02_method_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            percobaan2 objek = new percobaan2();
+            objek.pembagian(39, 35);
+            Console.WriteLine("Hasil pengurangan {0} dengan {1} adalah {2} ", 39, 35, objek.pengurangan(39, 35)); Console.ReadKey();
+        }
+    }
+}
